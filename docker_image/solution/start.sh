894ceae6ea90941d596fbe8ca97f79e1b227579b
Rihizz <santeri.riihela@gmail.com>
2023-10-16 13:28:48 +0300

    2.

--- docker_image/solution/start.sh
+++ docker_image/solution/start.sh
@@ -1,1 +1,1 @@
-../m1_game_engine -f ../maps/map00 -p1 ../m1_robots/bender -p2 ../m1_robots/bender -r
+../m1_game_engine -f ../maps/map00 -p1 ../solution/xdd/target/release/xdd -p2 ../m1_robots/bender -r
