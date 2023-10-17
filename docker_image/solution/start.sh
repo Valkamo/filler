2a0042347cf3e8802a536da8138236da5d26a38e
OskarV <ovalkamo@gmail.com>
2023-10-17 12:39:00 +0300

    p1info,pieceinfo

--- docker_image/solution/start.sh
+++ docker_image/solution/start.sh
@@ -1,1 +1,4 @@
+cd xdd
+cargo build --release
+cd ..
 ../m1_game_engine -f ../maps/map00 -p1 ../solution/xdd/target/release/xdd -p2 ../m1_robots/bender -r
--- docker_image/solution/xdd/src/main.rs
+++ docker_image/solution/xdd/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Read player information
+    let mut player_info = String::new();
+    io::stdin().read_line(&mut player_info).expect("Failed to read player info");
+
+    // Check if player_info contains "Answer" to skip printing it
+    if !player_info.contains("Answer") {
+        println!("{}", player_info.trim()); // Printing player info (optional)
+    }
+
+    // Read piece information
+    let mut piece_info = String::new();
+
+    // Read lines until an empty line is encountered
+    while io::stdin().read_line(&mut piece_info).expect("Failed to read piece info") > 0 {
+        if piece_info.trim().is_empty() {
+            println!("Empty line encountered, breaking out of the loop");
+            // Break out of the loop when an empty line is encountered
+            break;
+        }
+
+        // Print the current line of the piece information
+        println!("{}", piece_info.trim());
+
+        piece_info.clear(); // Clear the string for the next iteration
+    }
 }
